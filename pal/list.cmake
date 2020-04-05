8778738013e4d0b7f39afa3951ef11d864fbb43d
Sven Suursoho <sven@alt.ee>
2020-04-05 20:34:21 +0300

    Cooperative cancellation: cancel_source & cancel_token

--- pal/list.cmake
+++ pal/list.cmake
@@ -2,6 +2,7 @@ list(APPEND pal_sources
 	pal/__bits/lib
 	pal/__bits/platform_sdk
 	pal/assert
+	pal/cancel_token
 	pal/error
 	pal/error.cpp
 	pal/intrusive_mpsc_queue
@@ -14,6 +15,7 @@ list(APPEND pal_unittests_sources
 	pal/test
 	pal/test.cpp
 	pal/assert.test.cpp
+	pal/cancel_token.test.cpp
 	pal/error.test.cpp
 	pal/intrusive_mpsc_queue.test.cpp
 	pal/intrusive_queue.test.cpp
