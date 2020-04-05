20054e2a4d75befdccfef8d03abda74374ce5166
Sven Suursoho <sven@alt.ee>
2020-04-05 19:01:37 +0300

    Fix documentation generation

--- pal/assert.test.cpp
+++ pal/assert.test.cpp
@@ -5,24 +5,42 @@
 namespace {
 
 
+bool fn_with_side_effect (bool &side_effect, bool return_value)
+{
+	side_effect = true;
+	return return_value;
+}
+
+
 TEST_CASE("assert")
 {
+	bool side_effect = false;
+
 	SECTION("assert_true")
 	{
-		REQUIRE_NOTHROW(pal_assert(true));
+		REQUIRE_NOTHROW(
+			pal_assert(fn_with_side_effect(side_effect, true))
+		);
 	}
 
 	SECTION("assert_false")
 	{
 		if constexpr (pal::assert_noexcept)
 		{
-			REQUIRE_NOTHROW(pal_assert(false));
+			REQUIRE_NOTHROW(
+				pal_assert(fn_with_side_effect(side_effect, false))
+			);
 		}
 		else
 		{
-			REQUIRE_THROWS_AS(pal_assert(false), std::logic_error);
+			REQUIRE_THROWS_AS(
+				pal_assert(fn_with_side_effect(side_effect, false)),
+				std::logic_error
+			);
 		}
 	}
+
+	CHECK(side_effect == true);
 }
 
 
