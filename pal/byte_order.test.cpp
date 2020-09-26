929cc8af62dea6974db1ce23fb2c9bd6e96f1d84
Sven Suursoho <sven@alt.ee>
2020-09-26 10:03:35 +0300

    byte_order: host/network byte order conversion

---
+++ pal/byte_order.test.cpp
@@ -0,0 +1,53 @@
+#include <pal/byte_order>
+#include <pal/test>
+#include <limits>
+
+#if __pal_os_linux || __pal_os_macos
+	#include <arpa/inet.h>
+#elif __pal_os_windows
+	#include <winsock2.h>
+	#pragma comment(lib, "ws2_32")
+#endif
+
+#if __pal_os_linux
+	#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		#define htonll(v) __builtin_bswap64(v)
+		#define ntohll(v) __builtin_bswap64(v)
+	#else
+		#define htonll(v) (v)
+		#define ntohll(v) (v)
+	#endif
+#endif
+
+
+namespace {
+
+
+inline auto hton (uint16_t v) noexcept { return htons(v); }
+inline auto hton (uint32_t v) noexcept { return htonl(v); }
+inline auto hton (uint64_t v) noexcept { return htonll(v); }
+inline auto ntoh (uint16_t v) noexcept { return ntohs(v); }
+inline auto ntoh (uint32_t v) noexcept { return ntohl(v); }
+inline auto ntoh (uint64_t v) noexcept { return ntohll(v); }
+
+
+TEMPLATE_TEST_CASE("byte_order", "",
+	uint16_t,
+	uint32_t,
+	uint64_t)
+{
+	using limits = std::numeric_limits<TestType>;
+	auto value = GENERATE(
+		(limits::min)(),
+		(limits::lowest)(),
+		(limits::max)(),
+		static_cast<TestType>((limits::lowest)() + (limits::max)() / 2)
+	);
+	CAPTURE(value);
+
+	CHECK(hton(value) == pal::hton(value));
+	CHECK(ntoh(value) == pal::ntoh(value));
+}
+
+
+} // namespace
--- pal/list.cmake
+++ pal/list.cmake
@@ -1,6 +1,7 @@
 list(APPEND pal_sources
 	pal/__bits/lib
 	pal/__bits/platform_sdk
+	pal/byte_order
 	pal/error
 	pal/error.cpp
 	pal/expect
@@ -13,6 +14,7 @@ list(APPEND pal_sources
 list(APPEND pal_unittests_sources
 	pal/test
 	pal/test.cpp
+	pal/byte_order.test.cpp
 	pal/error.test.cpp
 	pal/expect.test.cpp
 	pal/intrusive_mpsc_queue.test.cpp
