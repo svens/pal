6a9eae0c01185f113de032dfd3dbc9820b09098c
Sven Suursoho <sven@alt.ee>
2020-10-23 20:57:46 +0300

    buffer: sequence access

--- pal/buffer.test.cpp
+++ pal/buffer.test.cpp
@@ -9,57 +9,72 @@ TEMPLATE_TEST_CASE("buffer", "",
 	pal::mutable_buffer,
 	pal::const_buffer)
 {
+	char data_1[1], data_2[2];
+	TestType
+		empty,
+		buffer_1(data_1, sizeof(data_1)),
+		buffer_2(data_2, sizeof(data_2));
+
 	SECTION("ctor")
 	{
-		TestType buffer;
-		CHECK(buffer.data() == nullptr);
-		CHECK(buffer.size() == 0);
+		CHECK(empty.data() == nullptr);
+		CHECK(empty.size() == 0);
 	}
 
 	SECTION("ctor(data, size)")
 	{
-		char buf[1];
-		TestType buffer(buf, sizeof(buf));
-		CHECK(buffer.data() == buf);
-		CHECK(buffer.size() == sizeof(buf));
+		CHECK(buffer_1.data() == data_1);
+		CHECK(buffer_1.size() == sizeof(data_1));
 	}
 
 	SECTION("ctor(that)")
 	{
-		char buf[1];
-		TestType a(buf, sizeof(buf));
-		TestType b(a);
-		CHECK(b.data() == buf);
-		CHECK(b.size() == sizeof(buf));
+		TestType a(buffer_1);
+		CHECK(a.data() == data_1);
+		CHECK(a.size() == sizeof(data_1));
 	}
 
-	if constexpr (std::is_same_v<TestType, pal::const_buffer>)
+	if constexpr (std::is_same_v<TestType, pal::mutable_buffer>)
 	{
 		SECTION("ctor(mutable_buffer)")
 		{
-			char buf[1];
-			pal::mutable_buffer a(buf, sizeof(buf));
-			TestType b(a);
-			CHECK(b.data() == buf);
-			CHECK(b.size() == sizeof(buf));
+			pal::const_buffer a(buffer_1);
+			CHECK(a.data() == data_1);
+			CHECK(a.size() == sizeof(data_1));
 		}
 	}
 
 	SECTION("+=")
 	{
-		char buf[2];
-		TestType buffer(buf, sizeof(buf));
+		TestType buffer(buffer_2);
 		buffer += 1;
-		CHECK(buffer.data() == &buf[1]);
-		CHECK(buffer.size() == sizeof(buf) - 1);
+		CHECK(buffer.data() == &data_2[1]);
+		CHECK(buffer.size() == sizeof(data_2) - 1);
 	}
 
 	SECTION("+= overflow")
 	{
-		char buf[2];
-		TestType buffer(buf, sizeof(buf));
-		buffer += 2*sizeof(buf);
+		TestType buffer(buffer_2);
+		buffer += 2 * sizeof(data_2);
+		CHECK(buffer.data() == &data_2[0] + 2);
 		CHECK(buffer.size() == 0);
+	}
+
+	SECTION("buffer_sequence_{begin,end}(1)")
+	{
+		CHECK(pal::buffer_sequence_begin(empty) == &empty);
+		CHECK(pal::buffer_sequence_end(empty) == &empty + 1);
+	}
+
+	SECTION("buffer_sequence_{begin,end}(N)")
+	{
+		TestType buffers[] = { empty, buffer_1, buffer_2 };
+		CHECK(pal::buffer_sequence_begin(buffers) == &buffers[0]);
+		CHECK(pal::buffer_sequence_end(buffers) == &buffers[3]);
+
+		TestType cbuffers[] = { empty, buffer_1, buffer_2 };
+		CHECK(pal::buffer_sequence_begin(cbuffers) == &cbuffers[0]);
+		CHECK(pal::buffer_sequence_end(cbuffers) == &cbuffers[3]);
 	}
 }
 
