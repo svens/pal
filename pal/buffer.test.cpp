773e3c5bb7ff46779967a036ba9da64146a71a82
Sven Suursoho <sven@alt.ee>
2020-10-24 10:57:01 +0300

    buffer: creation functions

--- pal/buffer.test.cpp
+++ pal/buffer.test.cpp
@@ -10,10 +10,7 @@ TEMPLATE_TEST_CASE("buffer", "",
 	pal::const_buffer)
 {
 	char data_1[1], data_2[2];
-	TestType
-		empty,
-		buffer_1(data_1, sizeof(data_1)),
-		buffer_2(data_2, sizeof(data_2));
+	TestType empty, buffer_1 = pal::buffer(data_1), buffer_2 = pal::buffer(data_2);
 
 	SECTION("ctor")
 	{
@@ -85,6 +82,346 @@ TEMPLATE_TEST_CASE("buffer", "",
 
 		TestType buffers[] = { empty, buffer_1, buffer_2 };
 		CHECK(pal::buffer_size(buffers) == 3);
+	}
+}
+
+
+TEST_CASE("buffer")
+{
+	SECTION("void *, size_t")
+	{
+		size_t size = sizeof(size_t);
+		void *data = &size;
+		auto buffer = pal::buffer(data, size);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data);
+		CHECK(buffer.size() == size);
+	}
+
+	SECTION("const void *, size_t")
+	{
+		size_t size = sizeof(size_t);
+		const void *data = &size;
+		auto buffer = pal::buffer(data, size);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data);
+		CHECK(buffer.size() == size);
+	}
+
+	/*
+	SECTION("int *, 0")
+	{
+		int data[2] = { 1, 2, };
+		auto buffer = pal::buffer(data, 0);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == nullptr);
+		CHECK(buffer.size() == 0);
+	}
+
+	SECTION("const int *, 0")
+	{
+		const int data[2] = { 1, 2, };
+		auto buffer = pal::buffer(data, 0);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == nullptr);
+		CHECK(buffer.size() == 0);
+	}
+	*/
+
+	SECTION("T (&)[Size]")
+	{
+		int data[2] = { 1, 2, };
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data);
+		CHECK(buffer.size() == sizeof(data));
+	}
+
+	SECTION("const T (&)[Size]")
+	{
+		const int data[2] = { 1, 2, };
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data);
+		CHECK(buffer.size() == sizeof(data));
+	}
+
+	SECTION("std::array<T, Size>")
+	{
+		std::array<int, 2> data = { 1, 2, };
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == sizeof(int) * data.size());
+	}
+
+	SECTION("std::array<const T, Size>")
+	{
+		std::array<const int, 2> data = { 1, 2, };
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == sizeof(int) * data.size());
+	}
+
+	SECTION("const std::array<T, Size>")
+	{
+		const std::array<int, 2> data = { 1, 2, };
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == sizeof(int) * data.size());
+	}
+
+	SECTION("std::vector<T>")
+	{
+		std::vector<int> data = { 1, 2, };
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == sizeof(int) * data.size());
+	}
+
+	SECTION("const std::vector<T>")
+	{
+		const std::vector<int> data = { 1, 2, };
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == sizeof(int) * data.size());
+	}
+
+	SECTION("std::string")
+	{
+		std::string data = "123";
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == data.size());
+	}
+
+	SECTION("const std::string")
+	{
+		const std::string data = "123";
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == data.size());
+	}
+
+	SECTION("std::string_view")
+	{
+		char buf[] = "123";
+		std::string_view data{buf};
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == data.size());
+	}
+
+	SECTION("std::wstring")
+	{
+		std::wstring data = L"123";
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == data.size() * sizeof(wchar_t));
+	}
+
+	SECTION("const std::wstring")
+	{
+		const std::wstring data = L"123";
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == data.size() * sizeof(wchar_t));
+	}
+
+	SECTION("std::wstring_view")
+	{
+		wchar_t buf[] = L"123";
+		std::wstring_view data{buf};
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == data.size() * sizeof(wchar_t));
+	}
+
+	SECTION("mutable_buffer")
+	{
+		char buf[] = "123";
+		const auto b1 = pal::buffer(buf);
+		auto b2 = pal::buffer(b1);
+		CHECK(std::is_same_v<decltype(b2), pal::mutable_buffer>);
+		CHECK(b2.data() == &buf[0]);
+		CHECK(b2.size() == sizeof(buf));
+	}
+
+	SECTION("mutable_buffer, size_t")
+	{
+		char buf[] = "123";
+		const auto b1 = pal::buffer(buf);
+		auto b2 = pal::buffer(b1, 1);
+		CHECK(std::is_same_v<decltype(b2), pal::mutable_buffer>);
+		CHECK(b2.data() == &buf[0]);
+		CHECK(b2.size() == 1);
+	}
+
+	SECTION("const_buffer")
+	{
+		const char buf[] = "123";
+		auto b1 = pal::buffer(buf);
+		auto b2 = pal::buffer(b1);
+		CHECK(std::is_same_v<decltype(b2), pal::const_buffer>);
+		CHECK(b2.data() == &buf[0]);
+		CHECK(b2.size() == sizeof(buf));
+	}
+
+	SECTION("const_buffer, size_t")
+	{
+		const char buf[] = "123";
+		auto b1 = pal::buffer(buf);
+		auto b2 = pal::buffer(b1, 1);
+		CHECK(std::is_same_v<decltype(b2), pal::const_buffer>);
+		CHECK(b2.data() == &buf[0]);
+		CHECK(b2.size() == 1);
+	}
+
+	SECTION("T (&)[Size], size_t")
+	{
+		int data[2] = { 1, 2, };
+		auto buffer = pal::buffer(data, 3);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data);
+		CHECK(buffer.size() == 3);
+	}
+
+	SECTION("const T (&)[Size], size_t")
+	{
+		const int data[2] = { 1, 2, };
+		auto buffer = pal::buffer(data, 3);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data);
+		CHECK(buffer.size() == 3);
+	}
+
+	SECTION("std::array<T, Size>, size_t")
+	{
+		std::array<int, 2> data = { 1, 2, };
+		auto buffer = pal::buffer(data, 3);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 3);
+	}
+
+	SECTION("std::array<const T, Size>, size_t")
+	{
+		std::array<const int, 2> data = { 1, 2, };
+		auto buffer = pal::buffer(data, 3);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 3);
+	}
+
+	SECTION("const std::array<T, Size>, size_t")
+	{
+		const std::array<int, 2> data = { 1, 2, };
+		auto buffer = pal::buffer(data, 3);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 3);
+	}
+
+	SECTION("std::vector<T>, size_t")
+	{
+		std::vector<int> data = { 1, 2, };
+		auto buffer = pal::buffer(data, 3);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 3);
+	}
+
+	SECTION("const std::vector<T>, size_t")
+	{
+		const std::vector<int> data = { 1, 2, };
+		auto buffer = pal::buffer(data, 3);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 3);
+	}
+
+	SECTION("std::string, size_t")
+	{
+		std::string data = "123";
+		auto buffer = pal::buffer(data, 2);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 2);
+	}
+
+	SECTION("const std::string, size_t")
+	{
+		const std::string data = "123";
+		auto buffer = pal::buffer(data, 2);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 2);
+	}
+
+	SECTION("std::string_view, size_t")
+	{
+		char buf[] = "123";
+		std::string_view data{buf};
+		auto buffer = pal::buffer(data, 2);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 2);
+	}
+
+	SECTION("std::wstring, size_t")
+	{
+		std::wstring data = L"123";
+		auto buffer = pal::buffer(data, 2);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 2);
+	}
+
+	SECTION("const std::wstring, size_t")
+	{
+		const std::wstring data = L"123";
+		auto buffer = pal::buffer(data, 2);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 2);
+	}
+
+	SECTION("std::wstring_view, size_t")
+	{
+		wchar_t buf[] = L"123";
+		std::wstring_view data{buf};
+		auto buffer = pal::buffer(data, 2);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == data.data());
+		CHECK(buffer.size() == 2);
+	}
+
+	SECTION("empty std::vector<T>")
+	{
+		std::vector<int> data;
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::mutable_buffer>);
+		CHECK(buffer.data() == nullptr);
+		CHECK(buffer.size() == 0);
+	}
+
+	SECTION("empty const std::vector<T>")
+	{
+		const std::vector<int> data;
+		auto buffer = pal::buffer(data);
+		CHECK(std::is_same_v<decltype(buffer), pal::const_buffer>);
+		CHECK(buffer.data() == nullptr);
+		CHECK(buffer.size() == 0);
 	}
 }
 
