6d076076f902c773d316db8557c2c0618beaf52c
Sven Suursoho <sven@alt.ee>
2020-11-16 18:56:10 +0200

    crypto: x509 OIDs

--- pal/crypto/list.cmake
+++ pal/crypto/list.cmake
@@ -3,6 +3,7 @@ list(APPEND pal_sources
 	pal/crypto/__bits/digest.cpp
 	pal/crypto/hash
 	pal/crypto/hmac
+	pal/crypto/oid
 	pal/crypto/random
 	pal/crypto/random.cpp
 )
@@ -11,5 +12,6 @@ list(APPEND pal_unittests_sources
 	pal/crypto/test
 	pal/crypto/hash.test.cpp
 	pal/crypto/hmac.test.cpp
+	pal/crypto/oid.test.cpp
 	pal/crypto/random.test.cpp
 )
