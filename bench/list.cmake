65fe55d7cba8895b7d291f3ed79a708c0989ce89
Sven Suursoho <sven@alt.ee>
2020-12-29 19:24:56 +0200

    bench: restore temporarily commented cases

--- bench/list.cmake
+++ bench/list.cmake
@@ -2,9 +2,9 @@ list(APPEND pal_benchmarks_sources
 	bench/main.cpp
 	bench/conv_base64.cpp
 	bench/conv_hex.cpp
-	#bench/crypto_hash.cpp
-	#bench/crypto_hmac.cpp
+	bench/crypto_hash.cpp
+	bench/crypto_hmac.cpp
 	bench/expected.cpp
-	#bench/hash.cpp
+	bench/hash.cpp
 	bench/invoke.cpp
 )
