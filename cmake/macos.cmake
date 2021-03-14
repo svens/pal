197b0a66d388bed083d6aa8d8863169e4492eb7f
Sven Suursoho <sven@alt.ee>
2021-03-14 16:26:48 +0200

    cmake: consume reusable macros from git module

--- cmake/macos.cmake
+++
@@ -1,9 +0,0 @@
-#
-# MacOS options
-#
-
-find_library(core_foundation_lib CoreFoundation)
-list(APPEND PAL_DEP_LIBS ${core_foundation_lib})
-
-find_library(security_lib Security)
-list(APPEND PAL_DEP_LIBS ${security_lib})
