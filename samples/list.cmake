e1a3e502073fc154dcbc7ab97380f9229846cd81
Sven Suursoho <sven@alt.ee>
2021-10-28 15:38:45 +0300

    samples/udp_relay_server_local_map: udp relay server with thread-specific map

--- samples/list.cmake
+++ samples/list.cmake
@@ -4,10 +4,20 @@ list(APPEND samples_common_sources
 	samples/thread
 )
 
+# udp_relay_server_global_map {{{1
 list(APPEND udp_relay_server_global_map
 	samples/udp_relay_server_global_map.cpp
 )
 cxx_executable(udp_relay_server_global_map
 	SOURCES ${udp_relay_server_global_map} ${samples_common_sources}
+	LIBRARIES ${pal_libraries}
+)
+
+# udp_relay_server_local_map {{{1
+list(APPEND udp_relay_server_local_map
+	samples/udp_relay_server_local_map.cpp
+)
+cxx_executable(udp_relay_server_local_map
+	SOURCES ${udp_relay_server_local_map} ${samples_common_sources}
 	LIBRARIES ${pal_libraries}
 )
