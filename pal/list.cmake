16379ebcabe769ba06cb6a6aea1731469e42921d
Sven Suursoho <sven@alt.ee>
2023-03-12 14:58:07 +0200

    byte_order: pal::ntoh, pal::hton

--- pal/list.cmake
+++ pal/list.cmake
@@ -1,5 +1,6 @@
 list(APPEND pal_sources
 	pal/__config
+	pal/byte_order
 	pal/error
 	pal/error.cpp
 )
@@ -7,5 +8,6 @@ list(APPEND pal_sources
 list(APPEND pal_test_sources
 	pal/test
 	pal/test.cpp
+	pal/byte_order.test.cpp
 	pal/error.test.cpp
 )
