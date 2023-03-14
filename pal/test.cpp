24316a2667912a543aaab0d50dba3605530071ec
Sven Suursoho <sven@alt.ee>
2023-03-14 15:40:46 +0200

    test: _CrtSetReportHook to print assertion message

--- pal/test.cpp
+++ pal/test.cpp
@@ -4,10 +4,34 @@
 #include <cstdlib>
 
 #include <pal/test>
+#include <pal/__config>
 #include <catch2/catch_session.hpp>
+
+#if __pal_os_windows && __pal_build_debug
+
+int report_hook (int report_type, char *message, int *return_value)
+{
+	static const char *level[] = { "WARN", "ERROR", "ASSERT" };
+	printf("[%s] %s\n", level[report_type], message);
+	*return_value = 0;
+	return true;
+}
+
+void set_report_hook ()
+{
+	_CrtSetReportHook(report_hook);
+}
+
+#else
+
+void set_report_hook ()
+{ }
+
+#endif
 
 int main (int argc, char *argv[])
 {
+	set_report_hook();
 	return Catch::Session().run(argc, argv);
 }
 
