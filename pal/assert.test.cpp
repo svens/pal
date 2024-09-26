26586be1189fefad26db093f708576a8ecb35cd8
Sven Suursoho <sven@alt.ee>
2024-09-26 20:24:28 +0300

    pal_assert

---
+++ pal/assert.test.cpp
@@ -0,0 +1,166 @@
+#include <pal/assert>
+#include <pal/test>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+
+namespace {
+
+bool bool_fn (bool &side_effect, bool return_value)
+{
+	side_effect = true;
+	return return_value;
+}
+
+TEST_CASE("assert")
+{
+	SECTION("bool")
+	{
+		bool side_effect = false;
+
+		SECTION("true")
+		{
+			REQUIRE_NOTHROW(pal_assert(bool_fn(side_effect, true)));
+			CHECK(pal_assert(bool_fn(side_effect, true)) == true);
+		}
+
+		SECTION("false")
+		{
+			if constexpr (pal::assert_can_throw)
+			{
+				REQUIRE_THROWS_AS(
+					pal_assert(bool_fn(side_effect, false)),
+					std::logic_error
+				);
+			}
+			else
+			{
+				REQUIRE_NOTHROW(pal_assert(bool_fn(side_effect, false)));
+				CHECK(pal_assert(bool_fn(side_effect, false)) == false);
+			}
+		}
+
+		CHECK(side_effect == true);
+	}
+
+	SECTION("pointer")
+	{
+		SECTION("not_nullptr")
+		{
+			char buf = 'a', *p = &buf;
+			REQUIRE_NOTHROW(pal_assert(p));
+			CHECK(pal_assert(p) == &buf);
+		}
+
+		SECTION("nullptr")
+		{
+			const char *p = nullptr;
+			if constexpr (pal::assert_can_throw)
+			{
+				REQUIRE_THROWS_AS(pal_assert(p), std::logic_error);
+			}
+			else
+			{
+				REQUIRE_NOTHROW(pal_assert(p));
+				CHECK(pal_assert(p) == nullptr);
+			}
+		}
+	}
+
+	SECTION("unique_ptr")
+	{
+		SECTION("not_nullptr")
+		{
+			auto buf = new char;
+			std::unique_ptr<char> p{buf};
+			REQUIRE_NOTHROW(pal_assert(p));
+			CHECK(pal_assert(p).get() == buf);
+		}
+
+		SECTION("nullptr")
+		{
+			std::unique_ptr<char> p;
+			if constexpr (pal::assert_can_throw)
+			{
+				REQUIRE_THROWS_AS(pal_assert(p), std::logic_error);
+			}
+			else
+			{
+				REQUIRE_NOTHROW(pal_assert(p));
+				CHECK(pal_assert(p) == nullptr);
+			}
+		}
+	}
+
+	SECTION("shared_ptr")
+	{
+		SECTION("not_nullptr")
+		{
+			auto buf = new char;
+			std::shared_ptr<char> p{buf};
+			REQUIRE_NOTHROW(pal_assert(p));
+			CHECK(pal_assert(p).get() == buf);
+		}
+
+		SECTION("nullptr")
+		{
+			std::shared_ptr<char> p;
+			if constexpr (pal::assert_can_throw)
+			{
+				REQUIRE_THROWS_AS(pal_assert(p), std::logic_error);
+			}
+			else
+			{
+				REQUIRE_NOTHROW(pal_assert(p));
+				CHECK(pal_assert(p) == nullptr);
+			}
+		}
+	}
+
+	SECTION("nullptr_t")
+	{
+		if constexpr (pal::assert_can_throw)
+		{
+			REQUIRE_THROWS_AS(pal_assert(nullptr), std::logic_error);
+		}
+		else
+		{
+			REQUIRE_NOTHROW(pal_assert(nullptr));
+			CHECK(pal_assert(nullptr) == nullptr);
+		}
+	}
+
+	if constexpr (pal::assert_can_throw)
+	{
+		using Catch::Matchers::ContainsSubstring;
+
+		SECTION("without message")
+		{
+			try
+			{
+				pal_assert(1 > 2);
+				FAIL("unexpected");
+			}
+			catch (const std::logic_error &e)
+			{
+				CHECK_THAT(e.what(), ContainsSubstring("1 > 2"));
+			}
+		}
+
+		SECTION("with message")
+		{
+			constexpr char message[] = "optional message";
+			try
+			{
+				pal_assert(1 > 2, message);
+				FAIL("unexpected");
+			}
+			catch (const std::logic_error &e)
+			{
+				CHECK_THAT(e.what(), ContainsSubstring(message));
+			}
+		}
+	}
+}
+
+
+} // namespace
--- pal/list.cmake
+++ pal/list.cmake
@@ -8,6 +8,7 @@ list(APPEND pal_sources ${CMAKE_CURRENT_BINARY_DIR}/version.cpp)
 list(APPEND pal_sources
 	pal/__expected
 	pal/__platform_sdk
+	pal/assert
 	pal/byte_order
 	pal/conv
 	pal/conv_base64.cpp
@@ -25,12 +26,13 @@ list(APPEND pal_sources
 list(APPEND pal_test_sources
 	pal/test
 	pal/test.cpp
+	pal/assert.test.cpp
 	pal/byte_order.test.cpp
 	pal/conv.bench.cpp
 	pal/conv.test.cpp
+	pal/error.test.cpp
 	pal/hash.bench.cpp
 	pal/hash.test.cpp
-	pal/error.test.cpp
 	pal/memory.test.cpp
 	pal/result.test.cpp
 	pal/span.test.cpp
