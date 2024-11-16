e1dbbeb6249105a38a8c85f0bb8c07bb6266af7d
Sven Suursoho <sven@alt.ee>
2024-11-16 10:57:21 +0200

    net/socket_base: move stuff from __socket namespace to socket_base

--- pal/net/socket_base.windows.cpp
+++ pal/net/socket_base.windows.cpp
@@ -90,11 +90,26 @@ const auto &lib_init = init();
 
 result<native_socket> open (int domain, int type, int protocol) noexcept
 {
-	if (auto h = ::socket(domain, type, protocol); h != __socket::invalid_handle)
+	if (auto h = ::socket(domain, type, protocol); h != native_socket_handle::invalid)
 	{
 		return native_socket_handle{h};
 	}
 	return __socket::sys_error();
+}
+
+struct socket_base::impl_type
+{
+	native_socket socket;
+};
+
+void socket_base::impl_type_deleter::operator() (impl_type *impl)
+{
+	delete impl;
+}
+
+const native_socket &socket_base::socket (const impl_ptr &impl) noexcept
+{
+	return impl->socket;
 }
 
 } // namespace pal::net
