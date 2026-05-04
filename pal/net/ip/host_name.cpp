985ff932f9a9f03f11a273cfe6b054a8e0193248
Sven Suursoho <sven@alt.ee>
2026-05-04 20:56:32 +0300

    pal/net/ip/host_name: clang-format

--- pal/net/ip/host_name.cpp
+++ pal/net/ip/host_name.cpp
@@ -6,7 +6,7 @@ namespace pal::net::ip
 
 result<std::string_view> host_name () noexcept
 {
-	static const auto name = []() noexcept -> result<std::string_view>
+	static const auto name = [] () noexcept -> result<std::string_view>
 	{
 		// NOLINTNEXTLINE(modernize-avoid-c-arrays,readability-magic-numbers)
 		static char buf[256] = {};
