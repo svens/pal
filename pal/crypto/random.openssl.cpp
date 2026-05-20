5ce4c6e992ab03072a6054c687bfd552da2d3efa
Sven Suursoho <sven@alt.ee>
2026-05-20 20:31:56 +0300

    pal/crypto: refactor random() API

--- pal/crypto/random.openssl.cpp
+++ pal/crypto/random.openssl.cpp
@@ -3,22 +3,19 @@
 #if __pal_crypto_openssl
 
 // clang-format off
-
 #include <pal/crypto/random.hpp>
 #include <openssl/rand.h>
+// clang-format on
 
 namespace pal::crypto::__crypto
 {
 
 result<void> random_fill (std::span<std::byte> buf) noexcept
 {
-	const auto rc = ::RAND_bytes(
-		reinterpret_cast<unsigned char *>(buf.data()),
-		static_cast<int>(buf.size())
-	);
+	const auto result = ::RAND_bytes(reinterpret_cast<unsigned char *>(buf.data()), static_cast<int>(buf.size()));
 
 	// LCOV_EXCL_START
-	if (rc != 1)
+	if (result != 1)
 	{
 		return make_unexpected(std::errc::io_error);
 	}
@@ -28,7 +25,5 @@ result<void> random_fill (std::span<std::byte> buf) noexcept
 }
 
 } // namespace pal::crypto::__crypto
-
-// clang-format on
 
 #endif // __pal_crypto_openssl
--- pal/crypto/random.test.cpp
+++ pal/crypto/random.test.cpp
@@ -2,35 +2,53 @@
 #include <pal/test.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <array>
+#include <cstdint>
 #include <string>
 
 namespace
 {
 
+struct uuid_t
+{
+	uint8_t data[16];
+	bool operator== (const uuid_t &) const = default;
+};
+
 TEST_CASE("crypto/random")
 {
-	const std::string zero(16, '\0');
-
-	SECTION("empty")
+	SECTION("buffer")
 	{
-		std::string buf;
-		REQUIRE(pal::crypto::random(buf));
-	}
-
-	SECTION("single")
-	{
+		const std::string zero(16, '\0');
 		auto buf = zero;
 		REQUIRE(pal::crypto::random(buf));
 		CHECK(buf != zero);
 	}
 
-	SECTION("multiple")
+	SECTION("buffer/empty")
 	{
-		std::string one = zero, two = zero;
-		std::array bufs = {std::span{one}, std::span{two}};
-		REQUIRE(pal::crypto::random(bufs));
-		CHECK(one != zero);
-		CHECK(two != zero);
+		std::string buf;
+		REQUIRE(pal::crypto::random(buf));
+	}
+
+	SECTION("trivially_copyable/uint32_t")
+	{
+		auto r = pal::crypto::random<uint32_t>();
+		REQUIRE(r);
+		CHECK(*r != 0);
+	}
+
+	SECTION("trivially_copyable/uint64_t")
+	{
+		auto r = pal::crypto::random<uint64_t>();
+		REQUIRE(r);
+		CHECK(*r != 0);
+	}
+
+	SECTION("trivially_copyable/struct")
+	{
+		auto r = pal::crypto::random<uuid_t>();
+		REQUIRE(r);
+		CHECK(*r != uuid_t{});
 	}
 }
 
