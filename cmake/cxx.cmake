bef9517ececdd4e07503ce99d0c3acb071fd5725
Sven Suursoho <sven@alt.ee>
2026-06-29 22:54:06 +0300

    cmake: make g++/clang++ build stop on first error

--- cmake/cxx.cmake
+++ cmake/cxx.cmake
@@ -5,6 +5,6 @@ set(CMAKE_CXX_SCAN_FOR_MODULES OFF)
 
 add_library(cxx_warnings INTERFACE)
 target_compile_options(cxx_warnings INTERFACE
-	$<$<CXX_COMPILER_ID:GNU,Clang>:-Werror -Wall -Wextra -Wnon-virtual-dtor -pedantic>
+	$<$<CXX_COMPILER_ID:GNU,Clang>:-Werror -Wall -Wextra -Wnon-virtual-dtor -Wpedantic -Wfatal-errors>
 	$<$<CXX_COMPILER_ID:MSVC>:/W4 /WX /w34265 /w34777 /w34946 /w35038>
 )
